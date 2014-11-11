bf91117c11f1635969a8222ca49bd9f1a220cc4d
Sarah Bizzarro <sarah@Sarahs-MacBook-Pro.local>
2014-11-10 22:32:50 -0500

    fixed strok

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <sys/stat.h>
 #include <string.h>
 #include "indexer.h"
 
@@ -10,44 +9,44 @@ int main (int argc, char const *argv[]) {
 		printf("invalid number of arguments");
 	}
 
-	if(fopen(argv[1], "r") == NULL) {
+	FILE *inverted = fopen(argv[1], "r");
+
+	if(inverted == NULL) {
 		printf("error - '%s' is not a file or directory\n", argv[1]);
 	    return -1;
 	}
 
-	FILE *inverted = fopen(argv[1], "r");
-	//Index *index = load_index(inverted);
+	Index *index = load_index(inverted);
 
 	char query[256];
-	scanf("%s", query);
 
 	while (1){
-		memset(&query[0], 0, sizeof(query));
-		scanf("%s", query);
+		fgets(query, 256, stdin);
+		printf("%s\n", query);
 		char* token = strtok(query, " ");
 		if (strcmp(token, "sa") == 0){
 			while (token != NULL){
-				token = strtok(NULL, " ");
+				printf("farto %s\n", token);
+				token = strtok(NULL, " \n");
 				/* insert 'and' function here */
-				break;
 			}
 		} else if (strcmp(token, "so") == 0){
 			while (token != NULL){
-				token = strtok(NULL, " ");
+				printf("%s\n", token);
+				token = strtok(NULL, " \n");
 				/* insert 'or' function here */
-				break;
 			}
-		} else if (strcmp(&query[0],"q") == 0){
+		} else if (query[0] == 'q' && query[1] == '\n'){
 			break;
 		} else {
-			printf("error - command not found");
-			break;
+			printf("error - command not found\n");
 		}
+		memset(query, 0, sizeof(query));
 	}
 
 
 	fclose(inverted);
-	//index_destroy(index);
+	index_destroy(index);
 
 	return 0;
 
